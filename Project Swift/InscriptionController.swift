1a99589f2bf56e4b87ff8b390fba31360d2e24ab
ophpkr <ophelie.piekarek@igtp4-mac07.isim.intra>
2018-03-13 16:03:41 +0100

    controllers

---
+++ Project Swift/InscriptionController.swift
@@ -0,0 +1,28 @@
+//
+//  inscriptionController.swift
+//  Project Swift
+//
+//  Created by Ophelie PIEKAREK on 13/03/2018.
+//  Copyright © 2018 Hamelina EHAMELO. All rights reserved.
+//
+
+import UIKit
+
+class InscriptionController: UIViewController {
+    
+    @IBAction func valider(_ sender: UIButton) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
